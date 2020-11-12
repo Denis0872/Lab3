ed1dd02839d663c9103009eb5602c33a1676bc16
denis0872 <denispetergof@gmail.com>
2020-11-12 12:19:48 +0300

    4

--- WhatDay/Program.cs
+++ WhatDay/Program.cs
@@ -8,11 +8,50 @@ namespace WhatDay
 {
     class Program
     {
-        static void Main(string[] args)
+        static string monthName;
+        //static int Day;
+        static string MetodMonth()
         {
             Console.WriteLine("Введите день от 1 до 365");
             string day = Console.ReadLine();
             int Day = int.Parse(day);
+            if (Day>=0&&Day<= 31)
+                monthName = "Januaru";
+            if (Day <= 59)
+                monthName = "February";
+            if (Day <= 90)
+                monthName = "Marth";
+            if (Day <= 121)
+                monthName = "Aptil";
+            if (Day <= 151)
+                monthName = "May";
+            if (Day <= 182)
+                monthName = "June";
+            if (Day <= 212)
+                monthName = "July";
+            if (Day <= 243)
+                monthName = "August";
+            if (Day <= 273)
+                monthName = "Stptember";
+            if (Day <= 304)
+                monthName = "October";
+            if (Day <= 334)
+                monthName = "November";
+            if (Day <= 365)
+                monthName = "December";
+            else
+            {
+                monthName = "Такого месяца не существует";
+            }
+            return monthName;
+            
         }
+        static void Main(string[] args)
+                    {
+                        MetodMonth();
+                        Console.WriteLine("{0}", monthName);
+                        Console.ReadKey();
+                    }
+
     }
 }
