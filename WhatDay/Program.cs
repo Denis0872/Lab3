deb192cdde257b7c904e2af39b423adda6434066
denis0872 <denispetergof@gmail.com>
2020-11-12 11:24:25 +0300

    1

---
+++ WhatDay/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите день от 1 до 365");
+            string day = Console.ReadLine();
+            int Day = int.Parse(day);
+        }
+    }
+}
