fdd5c445eb25096cc6525d9ff511ce11c2203e97
denis0872 <denispetergof@gmail.com>
2020-11-13 18:22:39 +0300

    поменял на foreach

--- WhatDay/Program.cs
+++ WhatDay/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,51 +9,69 @@ namespace WhatDay
 {
     class Program
     {
-        static string monthName;
+        enum MonthName {Januaru,February, Marth, Aptil,May,June,July,August,September, October,November,December };
+
         //static int Day;
-        static string MetodMonth()
-        {
+
+            #region old code
+            /*  if (Day<= 31)
+                  monthName = "Januaru";
+             else if (Day >= 31&&Day <= 59)
+                  monthName = "February";
+              else if(Day >= 59 && Day <= 90)
+                  monthName = "Marth";
+              else if (Day >= 90 && Day <= 121)
+                  monthName = "Aptil";
+              else if (Day >= 121 && Day <= 151)
+                  monthName = "May";
+              else if (Day >= 151 && Day <= 182)
+                  monthName = "June";
+              else if (Day >= 182 && Day <= 212)
+                  monthName = "July";
+              else if (Day >= 212 && Day <= 243)
+                  monthName = "August";
+              else if (Day >= 243 && Day <= 273)
+                  monthName = "Stptember";
+              else if (Day >= 273 && Day <= 304)
+                  monthName = "October";
+              else if (Day >= 304 && Day <= 334)
+                  monthName = "November";
+              else if (Day >= 334 && Day <= 365)
+                  monthName = "December";
+
+              else 
+              {
+                  monthName = "Такого месяца не существует";
+              }
+              return monthName;*/
+            #endregion
+        
+        static void Main(string[] args)
+                    {
+            ArrayList DaysInMonths = new ArrayList() { 31, 28, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30 };
             Console.WriteLine("Введите день от 1 до 365");
             string day = Console.ReadLine();
             int Day = int.Parse(day);
-            if (Day<= 31)
-                monthName = "Januaru";
-           else if (Day >= 31&&Day <= 59)
-                monthName = "February";
-            else if(Day >= 59 && Day <= 90)
-                monthName = "Marth";
-            else if (Day >= 90 && Day <= 121)
-                monthName = "Aptil";
-            else if (Day >= 121 && Day <= 151)
-                monthName = "May";
-            else if (Day >= 151 && Day <= 182)
-                monthName = "June";
-            else if (Day >= 182 && Day <= 212)
-                monthName = "July";
-            else if (Day >= 212 && Day <= 243)
-                monthName = "August";
-            else if (Day >= 243 && Day <= 273)
-                monthName = "Stptember";
-            else if (Day >= 273 && Day <= 304)
-                monthName = "October";
-            else if (Day >= 304 && Day <= 334)
-                monthName = "November";
-            else if (Day >= 334 && Day <= 365)
-                monthName = "December";
-
-            else 
+            int MonthNam = 0;
+            foreach (int daysInMonth in DaysInMonths)
             {
-                monthName = "Такого месяца не существует";
+                if (Day <= daysInMonth)
+                {
+                    break;
+                }
+                else
+                {
+                    Day -= daysInMonth;
+                    MonthNam++;
+                }
             }
-            return monthName;
-            
-        }
-        static void Main(string[] args)
-                    {
-                        MetodMonth();
-                        Console.WriteLine("{0}", monthName);
-                        Console.ReadKey();
-                    }
+            MonthName temp = (MonthName)MonthNam;
+            string MonthName = temp.ToString();
+
+            Console.WriteLine("{0} {1}", Day, MonthName);
+
+            Console.ReadKey();
+         }
 
     }
 }
