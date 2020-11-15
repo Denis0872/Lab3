05736ee063283e07a417baca8e904bfed62e7dd1
denis0872 <denispetergof@gmail.com>
2020-11-15 14:19:03 +0300

    Final version(added leap year list)

--- WhatDay/Program.cs
+++ WhatDay/Program.cs
@@ -48,16 +48,46 @@ namespace WhatDay
         
         static void Main(string[] args)
         {
-            try { 
-                    ArrayList DaysInMonths = new ArrayList() { 31, 28, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30 };
-                    Console.WriteLine("Введите день от 1 до 365");
+            try {
+                    Console.Write("Please enter the year: ");
+                    string line = Console.ReadLine();
+                    int yearNum = int.Parse(line);
+                bool isLeapYear = (yearNum % 4 == 0);
+
+                    if (isLeapYear)
+                    {
+                        Console.WriteLine(" IS a leap year");
+                    }
+                    else
+                    {
+                        Console.WriteLine(" is NOT a leap year");
+                    }
+                    int maxDayNum = isLeapYear ? 366 : 365;
+
+                    ArrayList DaysInMonths = new ArrayList() { 31, 28, 31, 30, 31, 31, 31, 30, 31, 30, 30, 31 };
+                    ArrayList DaysInMonthsLeap = new ArrayList() { 31, 29, 31, 30, 31, 31, 31, 30, 31, 30, 30, 31 };
+                    Console.WriteLine("Введите день от 1 до 365 или 366 если год високосный");
                     string day = Console.ReadLine();
                     int Day = int.Parse(day);
-                    if (Day < 1 || Day > 365)
+                    if (Day < 1 || Day > maxDayNum)
                     {
                     throw new ArgumentOutOfRangeException("Day out of range");
                     }
                     int MonthNam = 0;
+                     if(isLeapYear)
+                    foreach (int daysInMonth in DaysInMonthsLeap)
+                    {
+                        if (Day <= daysInMonth)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Day -= daysInMonth;
+                            MonthNam++;
+                        }
+                    }
+                     else
                     foreach (int daysInMonth in DaysInMonths)
                     {
                         if (Day <= daysInMonth)
@@ -70,7 +100,7 @@ namespace WhatDay
                             MonthNam++;
                         }
                     }
-                    MonthName temp = (MonthName)MonthNam;
+                MonthName temp = (MonthName)MonthNam;
                     string MonthName = temp.ToString();
 
                     Console.WriteLine("{0} {1}", Day, MonthName);
